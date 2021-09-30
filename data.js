3e2c05b9cccccf443d84ebeec54f072fb79db2bd
Caryn-Monash <91648753+Caryn-Monash@users.noreply.github.com>
2021-09-30 12:01:02 +0800

    init upload

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hokkien-cemetery",
+      "name": "Hokkien cemetery",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.07959458102301298,
+          "pitch": -0.22365486730027584,
+          "title": "Columbarium",
+          "text": "asdasdasdasd<div>asdasdasd</div><div>asdasdasd</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
